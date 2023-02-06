b6528a20aad7dd79863439aeb26ab30d9f615330
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-06 18:23:39 +0000

    feat: create basic routing without html and css

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
+    const {method} = req
+    console.log(method)
+    switch(method){
+        case 'GET':
+            return handleGet(req, res);
+        default:
+            res.statusCode = 501;
+            res.end(`The method can't be handled by the server: ${method}`);
+    }
     
-    res.end('hello');
 });
+
+function handleGet(req, res) {
+    const path = req.url;
+    if(path === '/'){
+        res.end('index')
+    } else if (path == '/about') {
+        res.end('/about')
+    } else if (path == '/contact-me') {
+        res.end('/contact')
+    }
+    res.statusCode = 404;
+    res.end('404')
+    
+}
 
 const PORT = 3000;
 
